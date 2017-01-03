bd5c54bc9ad3818d46b4df11e9e2dd9b80573d0e
luckyluke1994 <mddien1994@gmail.com>
2017-01-03 15:16:56 +0700

    Create Some Fake Data

--- app/src/main/java/app/com/example/maidaidien/sunshine/MainActivity.java
+++ app/src/main/java/app/com/example/maidaidien/sunshine/MainActivity.java
@@ -1,12 +1,16 @@
 package app.com.example.maidaidien.sunshine;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +32,17 @@ public class MainActivity extends AppCompatActivity {
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            // Create some dummy data for the ListView.  Here's a sample weekly forecast
+            String[] data = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
+            List<String> weekForecast = new ArrayList<>(Arrays.asList(data));
             View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
