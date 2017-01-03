8cce66263fe2cef98333bb88ce2ed1c3e76a2fdb
luckyluke1994 <mddien1994@gmail.com>
2017-01-03 16:19:53 +0700

    find ListView by method findViewById

--- app/src/main/java/app/com/example/maidaidien/sunshine/MainActivity.java
+++ app/src/main/java/app/com/example/maidaidien/sunshine/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -54,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
                     );
 
             View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
             return rootView;
         }
     }
