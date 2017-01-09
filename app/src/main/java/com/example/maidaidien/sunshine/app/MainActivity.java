8619f06955517eb67eae0a0d251c548d2ba482f1
luckyluke1994 <mddien1994@gmail.com>
2017-01-09 10:30:47 +0700

    Launch SettingsActivity

--- app/src/main/java/com/example/maidaidien/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/maidaidien/sunshine/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.maidaidien.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -26,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
         return super.onOptionsItemSelected(item);
     }
 }
